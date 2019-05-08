9f7a438316b834c87f43bdbdc26fe9993a59d751
Jacqueline Sayuri Michelucci Kiko [e] <jsayuri@fitec.org.br>
2019-05-08 09:55:41 -0300

    Adicionando lista de cursos

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal:string;
+
+  cursos: string[];
+
+ 
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://loiane.trainig'
+
+    /* 
+    for (let i=0; i<this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }     
+     */
+
+     this.cursos = this.cursosService.getCursos();
+     
+   }
 
   ngOnInit() {
   }
